fe59340597b1aad761a82f0eeb0e09684f8c8dfc
Sergio Pardo <sa.pardo10@uniandes.edu.co>
2018-05-10 17:49:27 -0500

    en redes

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -28,15 +28,22 @@ class App extends Component {
 				 for (let route of nestedBuses ) {
 			      	route.total = 0;
 			      	route.values[0].distance = 0;
+			      	
 			      	for (let i = 1 ; i < route.values.length; i++) {
+			      		
 			        	route.values[i].distance = getDistance(+route.values[i-1].lat, +route.values[i-1].lon,
 			          		+route.values[i].lat, +route.values[i].lon);
 			        	route.total += route.values[i].distance;
+			        	
 			    	}
+			    	
 				}
+				
  			return nestedBuses.sort(function(a, b) { return b.total - a.total; });
 		});
 	
+		console.log(nested);
+
 
 		this.margin = {top: 20, right: 20, bottom: 30, left: 40};
 
